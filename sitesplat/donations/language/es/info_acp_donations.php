d16ff0622c540f01c28b616350c7dc1879328b44
maxedrom <maxelocked@gmail.com>
2019-06-17 21:50:22 -0300

    info_acp_donations.php
    La traduccion de este archivo esta al 90 % Se de testear.

--- sitesplat/donations/language/es/info_acp_donations.php
+++ sitesplat/donations/language/es/info_acp_donations.php
@@ -1,6 +1,6 @@
 <?php
 /**
-*
+* @maxedrom [Español]
 * @package BBdonations
 * @copyright (c) 2016 SiteSplat All rights reserved
 * @license Proprietary
@@ -22,26 +22,26 @@ if ( empty($lang) || !is_array($lang) )
 
 $lang = array_merge($lang, array(
 	'ACP_DONATIONS'  				=> 'BBDonations',
-	'ACP_DONATIONS_EXPLAIN'			=> 'Donations settings for your website. Configure the desired settings, fill in the PayPal info and the Bitcoin info to maximize your donation results.',
-	'ACP_DONATIONS_INDEX_TITLE'		=> 'Config',
-	'ACP_DONATIONS_PAYPAL_EMAIL'	=> 'Paypal email',
-	'ACP_DONATIONS_PAYPAL_EXPLAIN'	=> 'Enter your PayPal email here. (leave empty to disable donation method) You might need to use PayPal business account as the IPN system might requires it to work correctly.<br /> For more information visit: <a href="https://developer.paypal.com/docs/classic/ipn/integration-guide/IPNSetup/#id089EG030E5Z">PayPal IPN setup</a>',
-	'ACP_DONATIONS_OPTIONS'			=> 'Donation options',
-	'ACP_DONATIONS_OPTIONS_EXPLAIN'	=> 'The following options are available: <br /> <strong>Sandbox Mode:</strong> Allows the sandbox mode (For both Bitcoin and PayPal) which is useful to test the donations. Please beaware that Coinbase sandbox is not reliable and therefore a real account should be used to run tests. The easiest way to test it is to use a smartphone app and donate back and forth between your forum and the app. Use the following app for android phones: <a href="https://airbitz.co/af/DJM" target="blank">AirBitz</a> <br /> <strong>Enable message:</strong> This enables the user to leave a short message with the donation <br /> <strong>Show donors:</strong> Enables the last recent donors',
-	'ACP_DONATIONS_SANDBOX'			=> 'Sandbox mode',
-	'ACP_DONATIONS_MESSAGE'			=> 'Enable message',
-	'ACP_DONATIONS_LIST'			=> 'Show donors',
+	'ACP_DONATIONS_EXPLAIN'			=> 'Configuración de donaciones para tu sitio web. Configure los ajustes deseados, complete la información de PayPal y la información de Bitcoin para maximizar los resultados de su donación.',
+	'ACP_DONATIONS_INDEX_TITLE'		=> 'Configurar',
+	'ACP_DONATIONS_PAYPAL_EMAIL'	=> 'Mail de Paypal',
+	'ACP_DONATIONS_PAYPAL_EXPLAIN'	=> 'Ingrese su correo electrónico de PayPal aquí. (déjelo vacío para deshabilitar el método de donación) Es posible que deba usar la cuenta comercial de PayPal ya que el sistema de IPN puede requerirlo para que funcione correctamente.<br /> Para mas información visite: <a href="https://developer.paypal.com/docs/classic/ipn/integration-guide/IPNSetup/#id089EG030E5Z">PayPal IPN setup</a>',
+	'ACP_DONATIONS_OPTIONS'			=> 'Opciones de Donaciones',
+	'ACP_DONATIONS_OPTIONS_EXPLAIN'	=> 'Las siguientes opciones se encuentran disponibles: <br /> <strong>Modo Sandbox:</strong> Permite el Modo SandBox (para Bitcoin y PayPal) el cual sirve para realizar tests de donaciones. Tenga en cuenta que el sandbox de Coinbase no es confiable y, por lo tanto, se debe utilizar una cuenta real para ejecutar pruebas. La forma más fácil de probarlo es usar una aplicación para teléfonos inteligentes y donar una y otra vez entre su foro y la aplicación. Use la siguiente aplicación para teléfonos android: <a href="https://airbitz.co/af/DJM" target="blank">AirBitz</a> <br /> <strong>Activar Mensaje:</strong> Esta opción permite que el usuario pueda dejar un mensaje corto con la doción realizada <br /> <strong>Mostrar Donadores:</strong> Activa una lista de los Donadores Recientes',
+	'ACP_DONATIONS_SANDBOX'			=> 'Modo Sandbox',
+	'ACP_DONATIONS_MESSAGE'			=> 'Activar Mensaje',
+	'ACP_DONATIONS_LIST'			=> 'Mostrar Donadores',
 	'ACP_DONATIONS_BITCOIN'			=> 'Bitcoin',
-	'ACP_DONATIONS_BITCOIN_EXPLAIN' => 'Enter Your coinbase API Key and API Secret here (leave empty to disable donation method). <br /> Follow <strong>EXACTLY</strong> (links open on a new tab) these steps to succesfully enable Bitcoin donation: <br /><strong>STEP1:</strong> <a href="https://www.coinbase.com/join/54c7b129d41344b592000006" target="_blank">Register a Coinbase account</a> <br /><strong>STEP2:</strong> <a href="https://www.coinbase.com/merchant_profiles" target="_blank">Setup the merchant page profile</a> <br /><strong>STEP3:</strong> <a href="https://www.coinbase.com/merchant_settings" target="_blank">Setup the merchant settings</a> <br /><strong>STEP4:</strong> In the notification URL option under Merchant settings (STEP3), enter your board URL like this:<br /> <code>http://www.my_board_url.com/app.php/donation/index.html?mode=notify_bitcoin</code> (Where "my_board_url.com" is your real board URL) <br /><strong>STEP5:</strong> Register the the Oauth application here: <a href="https://www.coinbase.com/settings/api" target="_blank">Oauth App</a>. Click on the button "+ New API key" then under "Accounts" checkmark "My Wallet (Primary)", under "Permissions" Click on select all and finally under "Notifications" enter the same URL as described on STEP4. Now click "Create" Button and get your API Key and API Secret to be enter here on your right. <br /><strong>STEP6:</strong> Congrats! Good luck with your donation campaign!',
+	'ACP_DONATIONS_BITCOIN_EXPLAIN' => 'Ingrese su API Key y la KEY secreta de coinbase (dejelo vacío para desactivar este metodo de donación). <br /> Siga <strong>EXACTAMENTE</strong> (los links se abriran en una nueva ventana) los pasos para activar correctamente la donación de Bitcoin: <br /><strong>PASO1:</strong> <a href="https://www.coinbase.com/join/54c7b129d41344b592000006" target="_blank">Registre una Cuenta de Coinbase</a> <br /><strong>PASO2:</strong> <a href="https://www.coinbase.com/merchant_profiles" target="_blank">Configure el perfil de la página del Comerciante.</a> <br /><strong>PASO3:</strong> <a href="https://www.coinbase.com/merchant_settings" target="_blank">Configure las Opciones del Comerciante</a> <br /><strong>PASO4:</strong> En la opción de URL de notificación en Configuración de comerciante (PASO 3), ingrese la URL de su tablero de esta forma<br /> <code>http://www.my_board_url.com/app.php/donation/index.html?mode=notify_bitcoin</code> (Donde "my_board_url.com" sea la URL de su sitio) <br /><strong>PASO5:</strong>Registre la Aplicacion de "Oauth" aquí: <a href="https://www.coinbase.com/settings/api" target="_blank">Aplicació de Autorización</a>. Clic en el botón "+ New API key" Luego en "Accounts" Clic en "My Wallet (Primary)", debajo de "Permissions" Clic en "select all" y para finalizar debajo de "Notifications" ingrese la misma URL como se describio en el PASO4. Ahora haga clic en el botón "Crear" y obtenga su clave API y su Clave Secreta API para ingresar aquí a su derecha. <br /><strong>PASO6:</strong> Felicidades Buena suerte con tu campaña de donaciones.!',
 	'ACP_DONATIONS_BITCOIN_KEY'		=> 'API Key',
 	'ACP_DONATIONS_BITCOIN_SECRET'	=> 'API Secret',
-	'ACP_DONATIONS_AMOUNTS'			=> 'Amounts',
-	'ACP_DONATIONS_TEXT'			=> 'Text',
-	'ACP_DONATIONS_SYMBOL'			=> 'Symbol',
-	'ACP_DONATIONS_ADD_ROW'			=> 'Add another row',
-	'ACP_DONATIONS_DELL_ROW'		=> 'Delete last row',
-	'ACP_DONATIONS_AMOUNTS_EXPLAIN'	=> 'The following options are available: (Use "%s" as a placeholder to fill in automatically the amount and currency in your text field.) <br /><strong>Amount:</strong> Set the donation amount <br /><strong>Text:</strong> Enter your custom text for the donation amount goal <br /><strong>Symbol:</strong> Select a font awesome symbol for each goal eantered.', 
-	'ACP_DONATIONS_STATUS'			=> 'Status',
+	'ACP_DONATIONS_AMOUNTS'			=> 'Cantidad',
+	'ACP_DONATIONS_TEXT'			=> 'Texto',
+	'ACP_DONATIONS_SYMBOL'			=> 'Símbolo',
+	'ACP_DONATIONS_ADD_ROW'			=> 'Añadir otra Fila',
+	'ACP_DONATIONS_DELL_ROW'		=> 'Eliminar Ultima Fila',
+	'ACP_DONATIONS_AMOUNTS_EXPLAIN'	=> 'Las Siguientes Opciones estan disponibles: (Utilize "%s" como un marcador de posición para completar automáticamente la cantidad y la moneda en su campo de texto.) <br /><strong>Cantidad:</strong> Ingrese la cantidad de la Donación <br /><strong>Texto:</strong> Ingrese su texto personalizado para el objetivo de cantidad de donación <br /><strong>Símbolo:</strong> Selecciones un simbolo font awesome para cada uno de los objetivos ingresados.', 
+	'ACP_DONATIONS_STATUS'			=> 'Estado',
 	'ACP_DONATIONS_GROUP'			=> 'Auto Group',
 	'ACP_DONATIONS_GROUP_EXPLAIN'	=> 'The following options are available for the AutoGroup settings: <br /><strong>Days:</strong> time expressed in days for the user to stay on X group. (No time disables fallback for this group) <br /> <strong>Amount:</strong> min currency (donation) value for the AutoGroup to be triggered. (0 disables AutoGroup) <br /><strong>Group:</strong> Group for the user to be moved into when the min. amount is triggered <br /><strong>Group Default:</strong> Set the group to default for the user (Checkmark to set as default)',
 	'ACP_DONATIONS_GROUP_DEF'		=> 'Group default',
